08c71d7068431a9efc143ae428d821759ec9a86e
Ranouel <nouraellm@gmail.com>
2019-05-07 02:30:13 +0000

    Home Controller

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -1,1 +1,27 @@
 <?php
+
+use \App\Controller as Controller;
+use \Models\Home as Home;
+
+/*
+ * HomeController Class
+ */
+class HomeController extends Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->model->call('Home');
+        $this->home = new Home;
+    }
+
+    public function index()
+    {
+        $this->view->msg = 'Lighty Framework';
+        $this->view->render('Welcome');
+    }
+    public function __404()
+    {
+        $this->view->msg = '404 Not Found';
+        $this->view->render('errors/index');
+    }
+}
