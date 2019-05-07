4c57443aec1e0a91d87ecce611fc42091a62d72b
AXeL-dev <contact.axel.dev@gmail.com>
2019-05-07 23:23:50 +0000

    code cleanup

--- app/Database.php
+++ app/Database.php
@@ -6,10 +6,15 @@ use MYSQLi;
 
 class Database extends MYSQLi
 {
+    /**
+     * Constructor
+     *
+     * @return $this
+     */
     public function __construct()
     {
         if (ENABLED) {
-            parent::__construct(HOST, USERNAME, PASSWORD, DATABASE );
+            parent::__construct(HOST, USERNAME, PASSWORD, DATABASE);
         }
     }
 }
--- app/Errors.php
+++ app/Errors.php
@@ -4,18 +4,27 @@ namespace App;
 
 use \App\View as View;
 
-/*
+/**
  * Errors Class
  */
-class Errors{
-
+class Errors
+{
+    /**
+     * Constructor
+     *
+     * @return $this
+     */
     public function __construct()
     {
         $this->index();
     }
+
+    /**
+     * Render errors/index view
+     */
     public function index()
     {
-        $this->view = new View;
+        $this->view = new View();
         $this->view->render('errors/index', true);
     }
 }
